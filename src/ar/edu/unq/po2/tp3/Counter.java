690b83e111dca436105770bbc7c04e2115bf30b0
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-03-30 19:52:42 -0300

    Inicio

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.po2.tp3;
+
+public class Counter {
+	
+	int [] numbers = new int[] {};
+	
+	public void testEvenNumbers() {
+		
+	}
+	
+	public void addNumber(int n) {
+		numbers [0] = n;
+	}
+	
+}
