af989b6f0f7de837d66e70403ef1d5aca331d29e
Ignacio <ignaciogarciareyna@live.com>
2023-04-03 16:32:05 -0300

    Punto de TP03 realizado.

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -1,15 +1,116 @@
 package ar.edu.unq.po2.tp3;
 
+import java.util.ArrayList;
+
 public class Counter {
-	
-	int [] numbers = new int[] {};
-	
+
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+
 	public void testEvenNumbers() {
-		
+
 	}
-	
-	public void addNumber(int n) {
-		numbers [0] = n;
+
+	public void addNumber(Integer n) {
+		numbers.add(n);
 	}
-	
+
+	private Boolean isMultipleOf(int n1, int n2) {
+		return n1 % n2 == 0;
+	}
+
+	private Boolean isEven(int number) {
+		return isMultipleOf(number, 2);
+	}
+
+	public Integer getEvenOcurrences() {
+		int evenOcurrences = 0;
+
+		for (int number : numbers) {
+			if (isEven(number)) {
+				evenOcurrences++;
+			}
+		}
+
+		return evenOcurrences;
+	}
+
+	public Integer getOddOcurrences() {
+		int oddOcurrences = 0;
+
+		for (int number : numbers) {
+			if (!isEven(number)) {
+				oddOcurrences++;
+			}
+		}
+
+		return oddOcurrences;
+	}
+
+	public Integer getMultiplesOfOcurrences(int number1) {
+		int multiplesOcurrences = 0;
+
+		for (int number2 : numbers) {
+			if (isMultipleOf(number2, number1)) {
+				multiplesOcurrences++;
+			}
+		}
+
+		return multiplesOcurrences;
+	}
+
+	private Integer evenDigitsOf(int n) {
+		int number = n;
+		int divisor = 10;
+		int evenDigits = 0;
+
+		while (number >= divisor) {
+			if (isEven(number % divisor)) {
+				evenDigits++;
+				number = number - (number % divisor);
+				divisor *= 10;
+			} else {
+				number = number - (number % divisor);
+				divisor *= 10;
+			}
+		}
+
+		return evenDigits;
+	}
+
+	private Boolean haveMoreEvenDigitsThan(int n1, int n2) {
+		return evenDigitsOf(n1) > evenDigitsOf(n2);
+	}
+
+	public Integer getMostEvenDigits(ArrayList<Integer> numbersDigits) {
+		int mostEvenDigits = 0;
+		for (int numberDigits : numbersDigits) {
+			if (haveMoreEvenDigitsThan(numberDigits, mostEvenDigits)) {
+				mostEvenDigits = numberDigits;
+			}
+		}
+		return mostEvenDigits;
+	}
+
+	// Realice una función que reciba dos valores X e Y, y devuelva el número más
+	// alto entre 0 y 1000, que sea
+	// múltiplo simultáneamente de X e Y. Si no existe, devolver -1. Por ejemplo si
+	// se ingresa 3 y 9, la función debe
+	// devolver 999.
+
+	public Boolean isSimultaneousMultiple(int multiple, int x, int y) {
+		return multiple % x == 0 && multiple % y == 0;
+	}
+
+	public Integer simultaneousMultipleLessThan1000(int x, int y) {
+		int simultaneousMultiple = 999;
+
+		while (!isSimultaneousMultiple(simultaneousMultiple, x, y) || simultaneousMultiple == 0) {
+			simultaneousMultiple--;
+		}
+		if (simultaneousMultiple == 0) {
+			return -1;
+		} else {
+			return simultaneousMultiple;
+		}
+	}
 }
--- test/ar/edu/unq/po2/tp3/CounterTestCase.java
+++ test/ar/edu/unq/po2/tp3/CounterTestCase.java
@@ -2,13 +2,93 @@ package ar.edu.unq.po2.tp3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class CounterTestCase {
+public class CounterTestCase {
+	private Counter counter;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	/**
+	 * Crea un escenario de test básico, que consiste en un contador con 10 enteros
+	 *
+	 * @throws Exception
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+//Se crea el contador
+		counter = new Counter();
+//Se agregan los numeros. Un solo par y nueve impares
+		counter.addNumber(1);
+		counter.addNumber(3);
+		counter.addNumber(5);
+		counter.addNumber(7);
+		counter.addNumber(9);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(4);
 	}
 
+	/**
+	 * Verifica la cantidad de pares
+	 */
+	@Test
+	public void testEvenNumbers() {
+// Getting the even occurrences
+		int amount = counter.getEvenOcurrences();
+// I check the amount is the expected one
+		assertEquals(amount, 1);
+	}
+	
+	/**
+	 * Verifica la cantidad de impares
+	 */
+	@Test
+	public void testOddNumbers() {
+// Getting the odd occurrences
+		int amount = counter.getOddOcurrences();
+// I check the amount is the expected one
+		assertEquals(amount, 9);
+	}
+	
+	/**
+	 * Verifica la cantidad de múltiplos de un número
+	 */
+	@Test
+	public void testMultiplesOfOcurrences() {
+// Getting the multiples occurrences
+		int amount = counter.getMultiplesOfOcurrences(3);
+// I check the amount is the expected one
+		assertEquals(amount, 2);
+	}
+	
+	
+	/**
+	 * Verifica la cantidad de múltiplos de un número
+	 */
+	@Test
+	
+	public void testMostEvenDigits() {
+		ArrayList<Integer> numbersDigits = new ArrayList<Integer>(Arrays.asList(11,1242,22334455,1111,22446688,1,231)) ;
+// Getting the multiples occurrences
+		int mostEvenDigits = counter.getMostEvenDigits(numbersDigits);
+// I check the amount is the expected one
+		assertEquals(mostEvenDigits, 22446688);
+	}
+	
+	/**
+	 * Verifica el múltiplo simultaneo menor a 1000 y mayor a 0 entre x e y
+	 */
+	@Test
+	
+	public void testSimultaneousMultipleLessThan1000() {
+// Getting the multiples occurrences
+		int simultaneousMultiple = counter.simultaneousMultipleLessThan1000(-2,99);
+// I check the amount is the expected one
+		assertEquals(simultaneousMultiple, 396);
+	}
 }
